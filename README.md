a46f55ce4c2b03ede7e5b6b3cd7ae79c2fba3c06
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-24 20:21:23 -0500

    pre-ejection commit

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Hangman game built with React
 
 
 ## Wireframe
-
 Preview with notes: https://wireframe.cc/PuHQGE
 
 ![desktop mockup](docs/img/mockup_desktop.png)
---
+++ src/containers/Hangman/Hangman.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Hangman extends Component {
+  state= {
+
+  }
+
+  render() {
+    return (
+      <p>hi</p>
+    )
+  }
+}
+
+export default Hangman
