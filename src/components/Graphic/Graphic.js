0045c088073561d9fcd9cadcabcb55478b35ac79
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-25 15:29:03 -0500

    sun graphic

--- src/components/Graphic/Graphic.js
+++ src/components/Graphic/Graphic.js
@@ -2,7 +2,16 @@ import React from 'react'
 import classes from './Graphic.css'
 
 const graphic = (props) => {
-  return (<div className={classes.Graphic}></div>)
+  const sunRays = []
+  for (let i = 0; i < props.playerIncorrectGuesses; i++) {
+    sunRays.push()
+  }
+
+  return (
+  <div className={classes.Graphic}>
+    <div className={classes.SunBody}></div>
+    <div className={[classes.SunRay, classes.SunRay1].join(' ')}></div>
+  </div>)
 }
 
 export default graphic
