253aa9ae40ac01b12e4213cad477e32bfba02f1d
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-25 22:27:54 -0500

    commit

--- src/components/CodedBy/CodedBy.js
+++ src/components/CodedBy/CodedBy.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import classes from './CodedBy.css'
 
 const codedBy = (props) => (
-  <p>Coded by <a href="http://www.aaronkwilkinson.com" target="_blank">AARON WILKINSON</a></p>
+  <p>Coded by <a href="http://www.aaronkwilkinson.com" target="_blank" rel="noopener noreferrer">AARON WILKINSON</a></p>
 )
 
 export default codedBy
