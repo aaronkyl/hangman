ff8d2f3e503956c6a916730376d6239785b12668
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-26 13:54:27 -0500

    cleaning up server code

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express')        //Express to run our app
 const app = express()                     //Initiate the app
-const bodyParser = require('body-parser') //Parse JSON requests
 const path = require('path')              //Navigate to build folder
 const axios = require('axios')
 
 // instruct Express to serve the files in the build folder
 app.use(express.static(path.join(__dirname, 'build')))
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-// })
 
-//API get request before serving React
+// API to get words from words API and avoid CORS issue that occures
+// when this external API is queried directly from the React app
 app.get('/api/getWords', (req, res) => {
-  console.log('api')
   const difficulty = req.query.difficulty > 0 ? '?difficulty=' + req.query.difficulty : ''
   axios.get('https://app.linkedin-reach.io/words' + difficulty)
     .then(response => {
